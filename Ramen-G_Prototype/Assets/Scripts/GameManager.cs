4eff19facbe8cf144ec623cafd420a653b8ba6ba
Quincy <Quincy.Lewis@mail.citytech.cuny.edu>
2022-02-23 20:44:37 -0500

    Prototype Added

---
+++ Ramen-G_Prototype/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    /*
+    [Header("Model References")]
+    public Model_Player playerModel;
+    public Model_Game gameModel;
+    [Header("Controller References")]
+    public Controller_Player playerController;
+    public Controller_PlayerBullet playerBulletController;
+    public Controller_Enemy enemyController;
+    public Controller_ShieldAndHealth shieldAndHealthcontroller;
+    [Header("CurrentState")]
+    [SerializeField] string state;
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Ramen-G_Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector2 direction = new Vector2(x, y).normalized;
+        CamMove(direction);
+    }
+
+    void CamMove(Vector2 direction)
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        max.x = max.x - 0.25f;
+        min.x = min.x + 0.25f;
+
+        max.y = max.y - 0.25f;
+        min.y = min.y + 0.25f;
+
+        Vector2 pos = transform.position;
+        pos += direction * speed * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, min.x, max.x);
+        pos.y = Mathf.Clamp(pos.y, min.y, max.y);
+
+        transform.position = pos;
+    }
+
+}

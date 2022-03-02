901770f0e41e366590f70f519c94b334d42c2a08
Alam1998 <shaiankhan77@gmail.com>
2022-03-02 11:26:38 -0500

    update1sk

--- Ramen-G_Prototype/Assets/Scripts/PlayerController.cs
+++ Ramen-G_Prototype/Assets/Scripts/PlayerController.cs
@@ -2,24 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+/* 
+ * Movement reference video link: https://www.youtube.com/watch?v=tH57EInEb58
+ * Moving background reference video link: https://www.youtube.com/watch?v=-6H-uYh80vc
+ * Background image site: https://wallpapersafari.com/w/NLfKEO
+ * 
+ */
+
+
+
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public float moveSpeed;
+    public Rigidbody2D rb2d;
+    private Vector2 moveInput;
 
-    // Start is called before the first frame update
+    private float activeMoveSpeed;
+    public float dashSpeed;
+
+    public float dashLength = .5f, dashCooldown = 1f;
+
+    private float dashCounter;
+    private float dashCoolCounter;
+    private float x;
+    private float y;
+
     void Start()
     {
-        
-
+        activeMoveSpeed = moveSpeed; 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
         Vector2 direction = new Vector2(x, y).normalized;
+
         CamMove(direction);
+
+        moveInput.Normalize();
+
+        rb2d.velocity = moveInput * activeMoveSpeed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (dashCoolCounter <= 0 && dashCounter <= 0)
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+            }
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if (dashCounter <= 0)
+            {
+                activeMoveSpeed = moveSpeed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+
+        if (dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
+        }
     }
 
     void CamMove(Vector2 direction)
@@ -34,12 +84,11 @@ public class PlayerController : MonoBehaviour
         min.y = min.y + 0.25f;
 
         Vector2 pos = transform.position;
-        pos += direction * speed * Time.deltaTime;
+        pos += direction * moveSpeed * Time.deltaTime;
 
         pos.x = Mathf.Clamp(pos.x, min.x, max.x);
         pos.y = Mathf.Clamp(pos.y, min.y, max.y);
 
         transform.position = pos;
     }
-
 }
